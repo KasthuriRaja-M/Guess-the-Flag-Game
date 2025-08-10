5f73193b5023b25f0a032f7b2d8f23ad0989ff57
KasthuriRaja-M <pmraja003@gmail.com>
2025-08-10 17:56:18 +0530

    Remove floating particle visual effects from App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,6 @@ import FlagGame from './components/FlagGame';
 const App: React.FC = () => {
   return (
     <div className="App">
-      {/* Floating particles for visual effect */}
-      <div className="floating-particle"></div>
-      <div className="floating-particle"></div>
-      <div className="floating-particle"></div>
-      <div className="floating-particle"></div>
       <FlagGame />
     </div>
   );
